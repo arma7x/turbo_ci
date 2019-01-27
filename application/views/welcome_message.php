827f3b72601d2602cc0bd881151c7128a2da981f
arma7x <arma7x@live.com>
2019-01-27 21:51:56 +0800

    house keeping

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -30,7 +30,7 @@
 			</div>
 		</div>
 	</div>
-	<h3 class="text-center text-primary">We make it simple but we built with powerful tools</h3>
+	<h3 class="text-center text-primary">We make it simple and we built with powerful tools</h3>
 	<div class="row">
 		<div class="col col-12 col-sm-3 mb-2">
 			<div class="card shadow-sm">
