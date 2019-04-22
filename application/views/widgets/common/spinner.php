26f1f7ef30a455380e96c3bcdf52d79a2a221330
arma7x <arma7x@live.com>
2019-04-22 12:07:05 +0800

    fixed spinner bg

--- application/views/widgets/common/spinner.php
+++ application/views/widgets/common/spinner.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
 
-    <div id="loading_spinner" class="spinner-gritcode">
+    <div id="loading_spinner" class="spinner-gritcode position-fixed">
         <div class="spinner-wrapper">
             <div class="spinner-border text-primary" role="status">
                 <span class="sr-only">Loading...</span>
